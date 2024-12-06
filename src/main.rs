9544d257e8fa8200e5683f83ae389016e0324ce7
Uriel Mandujano <5422638+konkolorado@users.noreply.github.com>
2024-12-05 18:19:35 -0600

    ft: update working example

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
+use std::{fs::File, io::Read};
+
+use dns_rs_lib::answer::Answer;
 use dns_rs_lib::header::Header;
 use dns_rs_lib::parser::print_bits;
-use dns_rs_lib::question::Question;
+use dns_rs_lib::records::A;
 
-use std::{fs::File, io::Read};
+use dns_rs_lib::question::Question;
 
 pub fn from_file(filename: &str) -> Vec<u8> {
     let mut buf: Vec<u8> = Vec::new();
@@ -12,21 +15,36 @@ pub fn from_file(filename: &str) -> Vec<u8> {
 }
 
 pub fn main() -> () {
-    let buf = from_file("query_packet.txt");
+    //let buf = from_file("query_packet.txt");
+    let buf = from_file("response_packet.txt");
 
     /*
     for i in 0..12 {
         print!("{}: ", i);
         print_bits(&buf[i]);
     }
-    let header = Header::from_buf(&buf);
-    dbg!(header);
     */
+    let ref header = Header::from_buf(&buf);
+    dbg!(header);
 
-    for i in 12..28 {
+    /*
+    for i in header._length..28 {
         print!("{}: ", i);
         print_bits(&buf[i]);
     }
-    let question = Question::from_buf(&buf);
+    */
+
+    let ref question = Question::from_buf(&buf[header._length..]);
     dbg!(question);
+
+    /*for i in header._length + question._length..header._length + question._length + 10 {
+        print!("{}: ", i);
+        print_bits(&buf[i]);
+    }
+    */
+    let ref answer = Answer::from_buf(&buf, header._length + question._length);
+    dbg!(answer);
+
+    let record = A::from_buf(&buf, header._length + question._length + answer._length);
+    dbg!(record);
 }
